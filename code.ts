94a664ba89374a8c3becb7bf9e2f2c8e5e4d828c
Milan Gladiš <hello@milangladis.com>
2023-02-17 23:12:30 +0100

    drag&drop, stroke weight based on size, color input improvements
    Refactored the plugin's code
    Fixed the position of the dropped icon
    Adjusted the stroke weight based on the icon's size
    Added a clickable information bubble with animation
    Improved the dark and light mode based on the colour's lightness
    Enhanced the colour input behaviour.

--- code.ts
+++ code.ts
@@ -4,17 +4,31 @@ figma.on('drop', (event: DropEvent) => {
   const { files, node, dropMetadata } = event;
 
   if (files.length > 0 && files[0].type === `image/svg+xml`) {
+    figma.ui.postMessage({ 
+      type: 'debug', 
+      msg: event
+    });
     files[0].getTextAsync().then((iconElement: string) => {
-      let newNode = figma.createNodeFromSvg(iconElement);
+      figma.ui.postMessage({ 
+        type: 'metadata', 
+        msg: dropMetadata
+      });
+      // console.log('page');
+      const newNode = figma.createNodeFromSvg(iconElement);
       
-      const name = dropMetadata.iconName;
-      // console.log(name);
-      newNode.x = event.x;
-      newNode.y = event.y;
+      const name = dropMetadata.name;
+      // console.log(event);
+      newNode.x = event.x - dropMetadata.size/2;
+      newNode.y = event.y - dropMetadata.size/2;
       newNode.name = name;
-      // node.appendChild(newNode);
-
-      figma.currentPage.selection = [newNode];
+      
+      node.appendChild(newNode);
+      if (node.appendChild) {
+        node.appendChild(newNode);
+      }
+      
+      // figma.currentPage.selection = [newNode];
+      
     });
 
     return false;
@@ -26,37 +40,11 @@ var settingsStroke:string;
 var settingsStrokeWeight:string;
 var settingsColor:string;
 
-// (async () => {
-//   try {
-//     settingsSize = await figma.clientStorage.getAsync('iconurSize');
-//     settingsStroke = await figma.clientStorage.getAsync('iconurStroke');
-//     settingsStrokeWeight = await figma.clientStorage.getAsync('iconurStrokeWeight');
-//     settingsColor = await figma.clientStorage.getAsync('iconurColor');
-//     banner = await figma.clientStorage.getAsync('iconurColor');
-
-//     if (settingsSize && settingsStroke && settingsStrokeWeight && settingsColor) {
-//       figma.ui.postMessage({
-// 				'type': 'settings',
-// 				'status': true,
-// 				'settingsStroke': settingsStroke,
-// 				'settingsSize': settingsSize,
-// 				'settingsStrokeWeight': settingsStrokeWeight,
-// 				'settingsColor': settingsColor,
-// 			});
-//     } else {
-//       returnEmptyStorage()
-//     }
-//   } catch (err) {
-//     returnEmptyStorage();
-//   }
-// })();
-
-
 (async () => {
   try {
     // figma.clientStorage.setAsync('iconurBannerBoolean', true);
     let iconurBannerBoolean = await figma.clientStorage.getAsync('iconurBannerBoolean');
-    console.log('storage: ' + iconurBannerBoolean)
+    // console.log('storage: ' + iconurBannerBoolean)
     if (iconurBannerBoolean !== undefined) {
       figma.ui.postMessage({ 
         type: 'banner', 
@@ -72,16 +60,8 @@ var settingsColor:string;
   }
 })();
 
-// function returnEmptyStorage() {
-//   figma.ui.postMessage({ 
-//     type: 'settings', 
-//     status: false 
-//   });
-// }
-
-
 figma.ui.onmessage = msg => {
-  console.log(msg)
+  // console.log(msg)
   if (msg.type === 'banner') {
     if (msg.status === false) {
       figma.clientStorage.setAsync('iconurBannerBoolean', false);
